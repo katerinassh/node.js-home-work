f85bd89aa9aeec46e50bd18100e32bc999b91c1f
Katerina <68004503+katerinassh@users.noreply.github.com>
2021-03-25 18:24:34 +0200

    ips between

---
+++ day 6.js
@@ -0,0 +1,10 @@
+const ipToBit = ([a, b, c, d]) => (+a * 256^3) + (+b * 256^2) + (+c * 256) + +d + 1;
+
+function ipsBetween(ip1, ip2) {
+    ip1 = ipToBit(ip1.split('.'));
+    ip2 = ipToBit(ip2.split('.'));
+    
+    return ip2 - ip1;
+}
+
+// console.log(ipsBetween("20.0.0.10", "20.0.1.0"));
